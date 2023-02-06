cfcb8364587c3331963d7b17bb7a69a68d88f28c
Daniel25778 <danielwilsonca94@gmailcom>
2023-02-06 11:19:44 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,4 +1,9 @@
 /* eslint-disable comma-dangle */
+import {
+  HttpResponse,
+  HttpStatusCode,
+} from '@/data/protocols/http/http-response'
+/* eslint-disable comma-dangle */
 /* eslint-disable space-before-function-paren */
 import {
   HttpPostClient,
@@ -8,10 +13,13 @@ import {
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent,
+  }
 
-  async post(params: HttpPostParams): Promise<void> {
+  async post(params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url
     this.body = params.body
-    return Promise.resolve()
+    return Promise.resolve(this.response)
   }
 }
--- src/data/usecase/authentication/remote-authentication.spec.ts
+++ src/data/usecase/authentication/remote-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { faker } from '@faker-js/faker'
 import { mockAuthentication } from '@/domain/test/mock-authentcation'
 /* eslint-disable comma-dangle */
@@ -5,6 +6,7 @@ import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 /* eslint-disable space-before-function-paren */
 
 import { RemoteAuthentication } from './remote-authentication'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credential-error'
 
 interface SutTypes {
   sut: RemoteAuthentication
@@ -32,5 +34,14 @@ describe('RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication()
     await sut.auth(authenticationParams)
     expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient return 401', async () => {
+    const { httpPostClientSpy, sut } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized,
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
