5aba6f18dd7009b114674ac26062753ca66983cd
Daniel25778 <danielwilsonca94@gmailcom>
2023-02-07 07:44:33 -0300

    feat: ensure AxiosHttpClient call axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+/* eslint-disable space-before-function-paren */
+import { HttpPostParams } from '@/data/protocols/http'
+export class AxiosHttpClient {
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios(params.url)
+  }
+}
