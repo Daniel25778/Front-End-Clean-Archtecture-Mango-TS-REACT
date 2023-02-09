5813e33e1ecbfdfa6e6568fb3cd74a8d5287b784
Daniel25778 <danielwilsonca94@gmailcom>
2023-02-09 08:56:09 -0300

    test: ensure Login will not render spinner and error on start

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,13 @@
 /* eslint-disable comma-dangle */
 module.exports = {
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/**/index.ts',
-    '!**/*.d.ts',
-  ],
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/tests/e2e/cypress',
-  ],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
     '\\.scss$': 'identity-obj-proxy',
   },
