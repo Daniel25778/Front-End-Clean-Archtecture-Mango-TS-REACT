3ac6a275238ec6e1a7294ae7a24655ef4fe754ed
Daniel25778 <danielwilsonca94@gmailcom>
2023-02-14 07:21:07 -0300

    test: prevent Login to call submit more than once

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -137,4 +137,11 @@ describe('Login Component', () => {
       password,
     })
   })
+
+  test('Should call Authentication with only once', () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+    expect(authenticationSpy.callsCount).toBe(1)
+  })
 })
